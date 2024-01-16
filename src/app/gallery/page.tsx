78dc0a4d19893651b0841230bd6138dafdd968af
pawas <pawas.saxena+1@gmail.com>
2024-01-16 16:54:52 +0530

    added basic css for the image gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -5,9 +5,14 @@ import ImageComponent from "@/components/Image";
 const Gallery = async () => {
   const { photos } = await getPhotos();
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
       {photos.map((photo: Photo) => (
-        <ImageComponent url={photo.src.large} alt={photo.alt} key={photo.id} />
+        <div
+          key={photo.id}
+          className="bg-white  overflow-hidden  rounded-lg  shadow-md"
+        >
+          <ImageComponent url={photo.src.large} alt={photo.alt} />
+        </div>
       ))}
     </div>
   );
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -9,7 +9,16 @@ type Props = {
 const ImageComponent: React.FC<Props> = ({ url, alt }: Props) => {
   return (
     <div>
-      <Image src={url} height={300} width={150} alt={alt} loader={()=> url}/>
+      <Image
+        src={url}
+        height={300}
+        width={150}
+        alt={alt}
+        loader={() => url}
+        unoptimized={true}
+        priority={true}
+        className="w-full h-48 object-cover object-center"
+      />
     </div>
   );
 };
