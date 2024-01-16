0092928db3bde9ef9dd703369ec8ab451e1baf27
pawas <pawas.saxena+1@gmail.com>
2024-01-16 17:58:27 +0530

    fixed style

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 const Grid: React.FC<Props> = ({ photos }: Props) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
       {photos.map((photo: Photo) => (
         <div
           key={photo.id}
